c93c6a8801618d217983765c2a3b78953de62160
Shubh Gupta <shubhorai12@gmail.com>
2025-06-23 15:28:11 +0530

    Refactor code structure for improved readability and maintainability

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -7,7 +7,7 @@ struct LinkedList{
         LinkedList* next;
 
         LinkedList(){
-            data = next = NULL;
+            next = NULL;
         }
         LinkedList(int val){
             data = val;
@@ -16,6 +16,7 @@ struct LinkedList{
 };
 int main(){
     LinkedList* head = new LinkedList();
+    LinkedList* tail = head;
     // cout<<head->data<<endl;
 
     // if(head->next==nullptr) cout<<"true";
@@ -27,7 +28,19 @@ int main(){
         cin>>vec[i];
     }
 
+    for(int i = 0; i<n; i++){
+        LinkedList* newnode = new LinkedList(vec[i]);
+        tail->next = newnode;
+        tail = newnode;
+    }
+    head = head->next;
+    LinkedList* curr = head;
+    while(curr){
+        cout<<curr->data;
+        curr = curr->next;
 
+    }
+    
 
 
 }
